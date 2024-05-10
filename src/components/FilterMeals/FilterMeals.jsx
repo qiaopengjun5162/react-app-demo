a4d3cbd5d90c663e5e98c2fe46064d70f58f0958
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-05-11 00:31:45 +0800

    FilterMeals update

--- src/components/FilterMeals/FilterMeals.jsx
+++ src/components/FilterMeals/FilterMeals.jsx
@@ -1,18 +1,29 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './FilterMeals.module.css'
 
 const FilterMeals = (props) => {
+    const [keyword, setKeyword] = useState('')
+
+    // 通过 useEffect 实现过滤
+    useEffect(() => {
+        props.onFilter(keyword)
+
+    }, [keyword, props])
 
     const keywordHandler = (e) => {
-        props.onFilter(e.target.value)
+        // props.onFilter(e.target.value)
+        setKeyword(e.target.value.trim())
     }
+
+
 
     return (
         <div className={classes.FilterMeals} >
             <div className={classes.InputOuter} >
                 <input
+                    value={keyword}
                     onChange={keywordHandler}
                     className={classes.SearchInput} type="text" placeholder="请输入关键字" />
                 <FontAwesomeIcon
